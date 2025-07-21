a3fa1457ff1e10fdcb9e7ff5685dc5f056ab765a
Alcides Ramos <alcides.ramos@leadtech.com>
2025-07-21 17:45:32 +0200

    refactor

--- tests/Unit/Providers/FooTest.php
+++ tests/Unit/Providers/FooTest.php
@@ -11,21 +11,10 @@ use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
-use SlopeIt\ClockMock\ClockMock;
 
 #[CoversClass(Foo::class)]
 final class FooTest extends TestCase
 {
-    protected function setUp(): void
-    {
-        ClockMock::freeze(new DateTime('2024-01-01 00:00:00'));
-    }
-
-    protected function tearDown(): void
-    {
-        ClockMock::reset();
-    }
-
     #[Test]
     #[DataProvider('dataProviderForGetDateTime')]
     public function checkGetDateTime(string $format): void
